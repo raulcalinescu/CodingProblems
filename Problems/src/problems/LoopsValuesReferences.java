b060305b77f8c3b2efbd97d85e317d587fe3531a
Raul Salajan <rcsalaja@us.ibm.com>
2018-08-11 11:19:56 -0500

    Adding files from worklaptop

---
+++ Problems/src/problems/LoopsValuesReferences.java
@@ -0,0 +1,40 @@
+package problems;
+
+import com.sun.xml.internal.xsom.impl.scd.Iterators;
+
+import java.lang.reflect.Array;
+import java.util.*;
+import java.io.*;
+import java.lang.Math;
+
+class LoopsValuesReferences{
+
+    public static void printSeries(int a, int b, int n) {
+        int sum = a;
+
+        for (int i =0; i < n; i++) {
+            sum = sum + b * (int)Math.round(Math.pow(2,i));
+            System.out.print(sum+" ");
+        }
+        System.out.print("\n");
+    }
+
+    public static void main(String []argh){
+        ArrayList <int[]> queryValues = new ArrayList<int[]>();
+        int threeVal[];
+
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+
+        for(int i=0;i<t;i++){
+            queryValues.add(i,new int[]{in.nextInt(),in.nextInt(),in.nextInt()});
+        }
+
+        for(int i = 0; i < t; i++) {
+            threeVal = queryValues.get(i);
+            printSeries(threeVal[0],threeVal[1],threeVal[2]);
+        }
+
+        in.close();
+    }
+}
