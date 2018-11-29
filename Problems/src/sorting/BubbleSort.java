0d11cae2dcad4df10b56c4036cd4d896414f0157
Raul Salajan <rcsalaja@us.ibm.com>
2018-11-29 11:42:05 -0600

    Modify code from worklaptop

---
+++ Problems/src/sorting/BubbleSort.java
@@ -0,0 +1,46 @@
+package sorting;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class BubbleSort {
+
+    // Complete the countSwaps function below.
+    static void countSwaps(int[] a) {
+        int numSwaps = 0;
+
+        for (int i = 0; i < a.length - 1; i++)
+            for (int j=1; j < a.length; j++)
+
+        System.out.println("Array is sorted in " + numSwaps + "swaps.");
+        System.out.println("First Element: " + a[0]);
+        System.out.println("Last Element: " + a[a.length-1]);
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] a = new int[n];
+
+        String[] aItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a[i] = aItem;
+        }
+
+        countSwaps(a);
+
+        scanner.close();
+    }
+}
