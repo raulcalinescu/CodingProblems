95be4c90bc5e38a026f0685d6af06427e578eeb7
Raul Salajan <rcsalaja@us.ibm.com>
2018-11-30 17:44:50 -0600

    Modify code from worklaptop

--- Problems/src/strings/ValidIfSameCountCharacters.java
+++ Problems/src/strings/ValidIfSameCountCharacters.java
@@ -12,36 +12,48 @@ public class ValidIfSameCountCharacters {
 
     // Complete the isValid function below.
     static String isValid(String s) {
-        int[] counts = new int[]{};
-        Collection <Integer> values;
-        Map chars = new HashMap<String,Integer>();
+        int cnt = 0;
+        boolean firsTime = true, valid = true;
+        Collection<Integer> values;
+        Map chars = new HashMap<String, Integer>();
         int temp = 0;
 
         char[] c = s.toCharArray();
 
         for (int i = 0; i < c.length; i++)
-            if(chars.containsKey(c[i]))
-                chars.put(c[i],(int)chars.get(c[i]) + 1);
+            if (chars.containsKey(c[i]))
+                chars.put(c[i], (int) chars.get(c[i]) + 1);
             else
-                chars.put(c[i],1);
+                chars.put(c[i], 1);
 
-      values = chars.values();
-      Integer[] targetArray = values.toArray(new Integer[values.size()]);
+        values = chars.values();
+        Integer[] targetArray = values.toArray(new Integer[values.size()]);
 
-      for (Integer i : targetArray) {
-          if (Math.abs(temp - i.intValue()) == 1)
+        for (Integer i : targetArray) {
+            if (i == targetArray[0]) {
+                cnt = targetArray[0];
+                continue;
+            }
 
-
-
-
-      }
-
-
-
-
-
-        return "YES";
+            if (i == cnt) continue;
+            else if (i == 1 && firsTime) {
+                firsTime = false;
+                continue;
+            }
+            else if (i == cnt+1 && firsTime) {
+                firsTime = false;
+                continue;
+            }
+            else {
+                valid = false;
+                break;
+            }
+        }
+        return valid ? "YES" : "NO";
     }
+
+
+
 
     private static final Scanner scanner = new Scanner(System.in);
 
