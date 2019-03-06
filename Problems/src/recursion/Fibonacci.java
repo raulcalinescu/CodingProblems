b9e57c8899885cddfaec24368ec4321f9883aa1b
Raul Salajan <rcsalaja@us.ibm.com>
2019-03-06 10:52:47 -0600

    Modify code from worklaptop

--- Problems/src/recursion/Fibonacci.java
+++ Problems/src/recursion/Fibonacci.java
@@ -2,6 +2,9 @@ package recursion;
 
 import java.util.*;
 
+// given a number n, find the nth number in the fibonacci sequence
+// f(0) = 0, f(1) = 1, and then f(n) = f(n-1) + f(n-2)
+
 public class Fibonacci {
 
     public static int fibonacci(int n) {
@@ -11,7 +14,7 @@ public class Fibonacci {
         } else if (n == 1) {
             return 1;
         } else {
-            return fibonacci(n-1 + n-2);
+            return fibonacci(n-1) + fibonacci (n-2);
         }
     }
 
