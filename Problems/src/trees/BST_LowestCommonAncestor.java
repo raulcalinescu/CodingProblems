7c665136203b81dad22dfc2d45db4fd4d8b77b65
raulcalinescu <raul.salajan@gmail.com>
2019-03-12 00:00:11 -0500

    Code added from Personal laptop

--- Problems/src/trees/BST_LowestCommonAncestor.java
+++ Problems/src/trees/BST_LowestCommonAncestor.java
@@ -1,14 +1,47 @@
 package trees;
+
+
 import java.util.*;
 import java.io.*;
+
+
 
 public class BST_LowestCommonAncestor {
 
     public static Node lca(Node root, int v1, int v2) {
-        // Write your code here.
+        if (root.data < v1 && root.data < v2) {
+            return lca(root.right, v1, v2);
+        } else if (root.data > v1 && root.data > v2) {
+            return lca(root.left, v1, v2);
+        } else {
+            return root;
+        }
 
-        return root;
+
     }
+
+    /* performance improvement:
+
+    node* lca_(node* root, int a, int b)
+{
+    if (b < root->data) {
+        return lca_(root->left, a, b);
+    }
+    else if (a > root->data) {
+        return lca_(root->right, a, b);
+    }
+    return root;
+}
+
+node* lca(node* root, int a, int b)
+{
+    if (a < b) {
+        return lca_(root, a, b);
+    }
+    return lca_(root, b, a);
+}
+
+     */
 
     public static Node insert(Node root, int data) {
         if(root == null) {
