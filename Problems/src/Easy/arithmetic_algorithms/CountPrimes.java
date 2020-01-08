edadd98958dafb4de7006e29272d0220a46e6ba2
Raul <rcsalaja@us.ibm.com>
2020-01-08 11:01:32 -0600

    WorkLaptop

--- Problems/src/Easy/arithmetic_algorithms/CountPrimes.java
+++ Problems/src/Easy/arithmetic_algorithms/CountPrimes.java
@@ -2,19 +2,40 @@ package Easy.arithmetic_algorithms;
 
 /*
 Count the number of prime numbers less than a non-negative number, n.
+
+ n = 6;  3 primes
+ 2, 3, 5
+
+ n = 10; 4 primes
+ 2, 3, 5, 7
+
  */
 
 public class CountPrimes {
-    public int countPrimes(int n) {
+
+    // time limit exceeded for this Solution at input n = 499979. my runtime O(n^2)
+    // Should have used the Sieve Eratosthenes' Method
+
+    /*
+    Create a list of consecutive integers from 2 through n: (2, 3, 4, ..., n).
+    Initially, let p equal 2, the smallest prime number.
+    Enumerate the multiples of p by counting in increments of p from 2p to n, and mark them in the list
+            (these will be 2p, 3p, 4p, ...; the p itself should not be marked).
+    Find the first number greater than p in the list that is not marked. If there was no such number, stop.
+        Otherwise, let p now equal this new number (which is the next prime), and repeat from step 3.
+    When the algorithm terminates, the numbers remaining not marked in the list are all the primes below n.
+
+    public static int countPrimes(int n) {
         int primes = 0;
-        while (n != 0) {
-            if(isPrime(n))
+        while (n > 1) {
+            if(isPrime(n-1))
                 primes++;
+                n--;
         }
         return primes;
     }
 
-    public boolean isPrime(int num) {
+    public static boolean isPrime(int num) {
         if(num == 0 || num == 1) {
             return false;
         }
@@ -25,4 +46,8 @@ public class CountPrimes {
         }
         return true;
     }
+
+    public static void main(String[] args) {
+        System.out.println(countPrimes(6));
+    } */
 }
