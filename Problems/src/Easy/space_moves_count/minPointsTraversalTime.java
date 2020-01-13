110b8b7e90fe6ab9b6a5f591290dc23400a76434
Raul <rcsalaja@us.ibm.com>
2020-01-13 14:40:19 -0600

    WorkLaptop

--- Problems/src/Easy/space_moves_count/minPointsTraversalTime.java
+++ Problems/src/Easy/space_moves_count/minPointsTraversalTime.java
@@ -14,20 +14,17 @@ You have to visit the points in the same order as they appear in the array.
 
 public class minPointsTraversalTime {
     public static int minTimeToVisitAllPoints(int[][] points) {
-        if(points.length == 1 || points.length == 0)
+        if(points.length == 1 || points.length == 0)   //length of a 2D array is equal to the number of rows (i)
             return 0;
 
         if(points.length == 2)
             return (int) Math.sqrt((Math.pow(points[1][0] - points[0][0],2) +
                     Math.pow(points[1][1] - points[0][1],2)));
 
-        int min_time = 0, x1 = 0, x2 =0, y1=0, y2=0;
-        for(int i = 0; i < points.length; i++) {
-            for(int j = 0; j < points.length; j++) {
-                 points[i][j];
-
-                 break;
-            }
+        int min_time = 0;
+        for(int i = 0; i < points.length - 1; i++) {
+            min_time += (int) Math.sqrt((Math.pow(points[i+1][0] - points[i][0],2) +
+                    Math.pow(points[i+1][1] - points[i][1],2)));
         }
         return min_time;
     }
@@ -35,7 +32,7 @@ public class minPointsTraversalTime {
     public static void main(String[] args) {
         int[][] mat3 = {{1,1},{3,4},{-1,0}};
         int[][] mat2 = {{1,1},{3,4}};
-        System.out.println(minTimeToVisitAllPoints(mat2));
+        System.out.println(minTimeToVisitAllPoints(mat3));
 
     }
 }
