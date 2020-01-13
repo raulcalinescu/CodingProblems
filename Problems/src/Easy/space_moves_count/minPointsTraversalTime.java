07e5c9919c03d4f255236823db8581af49941e83
Raul <rcsalaja@us.ibm.com>
2020-01-13 10:51:28 -0600

    WorkLaptop

---
+++ Problems/src/Easy/space_moves_count/minPointsTraversalTime.java
@@ -0,0 +1,31 @@
+package Easy.space_moves_count;
+
+/*
+On a plane there are n points with integer coordinates points[i] = [xi, yi].
+Your task is to find the minimum time in seconds to visit all points.
+
+You can move according to the next rules:
+
+In one second always you can either move vertically, horizontally by one unit or diagonally
+(it means to move one unit vertically and one unit horizontally in one second).
+
+You have to visit the points in the same order as they appear in the array.
+ */
+
+public class minPointsTraversalTime {
+    public int minTimeToVisitAllPoints(int[][] points) {
+        int time = 0;
+        for(int i = 0; i < points.length; i++) {
+            for(int j = 0; j < points.length; j++) {
+                points[i][j]
+            }
+        }
+        return time;
+    }
+
+    public static void main(String[] args) {
+        int[][] mat = {{1,1},{3,4},{-1,0}};
+
+
+    }
+}
