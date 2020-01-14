6a7f0badbf13362756373e395dc7a54d884fdbad
Raul <rcsalaja@us.ibm.com>
2020-01-14 12:23:37 -0600

    WorkLaptop

--- Problems/src/Easy/array_manipulation/DecompressEncodedArray.java
+++ Problems/src/Easy/array_manipulation/DecompressEncodedArray.java
@@ -1,7 +1,27 @@
 package Easy.array_manipulation;
 
+
+/*
+Integer array concatenation:
+
+int[] array1 = {1, 2, 3};
+        int[] array2 = {4, 5, 6};
+        int aLen = array1.length;
+        int bLen = array2.length;
+        int[] result = new int[aLen + bLen];
+        System.arraycopy(array1, 0, result, 0, aLen);
+        System.arraycopy(array2, 0, result, aLen, bLen);
+
+ */
+
 public class DecompressEncodedArray {
     public static int[] decompressRLElist(int[] nums) {
+        for(int i = 0; i < nums.length; i+=2) {
+            while(nums[i] > 0) {
+
+                nums[i]--;
+            }
+        }
         return nums;
     }
 
