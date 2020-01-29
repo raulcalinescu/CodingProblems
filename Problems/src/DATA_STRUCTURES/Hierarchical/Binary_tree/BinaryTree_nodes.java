9ae8dbbf9ef28fbd9f2c915c3e8a0479c5e03f3e
raulcalinescu <raul.salajan@gmail.com>
2020-01-28 19:05:09 -0600

    Code added from Personal laptop

--- Problems/src/DATA_STRUCTURES/Hierarchical/Binary_tree/BinaryTree_nodes.java
+++ Problems/src/DATA_STRUCTURES/Hierarchical/Binary_tree/BinaryTree_nodes.java
@@ -1,4 +1,52 @@
 package DATA_STRUCTURES.Hierarchical.Binary_tree;
+/*
+
+Main applications of trees include:
+1. Manipulate hierarchical data.
+2. Make information easy to search (see tree traversal).
+3. Manipulate sorted lists of data.
+4. As a workflow for compositing digital images for visual effects.
+5. Router algorithms
+6. Form of a multi-stage decision-making (see business chess).
+
+
+Depth First Traversals:
+(a) Inorder (Left, Root, Right) : 4 2 5 1 3
+(b) Preorder (Root, Left, Right) : 1 2 4 5 3
+(c) Postorder (Left, Right, Root) : 4 5 2 3 1
+
+Breadth First or Level Order Traversal : 1 2 3 4 5
+
+ */
+
+import com.sun.source.tree.BinaryTree;
+import com.sun.source.tree.Tree;
 
 public class BinaryTree_nodes {
+    TreeNode root;
+
+    BinaryTree_nodes(int data) {
+        root = new TreeNode(data);
+    }
+
+    BinaryTree_nodes() {
+        root = null;
+    }
+
+    public void printBreathFirstTraversal(){   // level order traversal
+
+    }
+
+    public static void main(String[] args) {
+        BinaryTree_nodes BT = new BinaryTree_nodes();
+        BT.root = new TreeNode(20);
+        BT.root.rightChild = new TreeNode(8);
+        BT.root.leftChild = new TreeNode(12);
+        BT.root.leftChild.leftChild = new TreeNode(4);
+        BT.root.leftChild.rightChild = new TreeNode(4);
+        BT.root.rightChild.leftChild = new TreeNode(6);
+        BT.root.rightChild.rightChild = new TreeNode(6);
+
+
+    }
 }
