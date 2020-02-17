fd9abcd3e558ee52ffff4d38e1da84302731fe9c
raulcalinescu <raul.salajan@gmail.com>
2020-02-16 18:39:05 -0600

    Code added from Personal laptop

---
+++ Problems/src/HARD/Lists_Manipulation/MergeSortedLists.java
@@ -0,0 +1,40 @@
+package HARD.Lists_Manipulation;
+
+/*
+Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+
+Example:
+
+Input:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+Output: 1->1->2->3->4->4->5->6
+
+ */
+
+public class MergeSortedLists {
+
+     // Definition for singly-linked list.
+     private class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+     }
+
+    public ListNode mergeKLists(ListNode[] lists) {
+         ListNode result;
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
