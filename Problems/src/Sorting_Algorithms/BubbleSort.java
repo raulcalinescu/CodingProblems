9f04fd45b1a675e17e06a11c9d4b4ce2deca3f47
raulcalinescu <raul.salajan@gmail.com>
2020-02-18 09:03:32 -0600

    Code added from Personal laptop

--- Problems/src/Sorting_Algorithms/BubbleSort.java
+++ Problems/src/Sorting_Algorithms/BubbleSort.java
@@ -5,6 +5,28 @@ public class BubbleSort {
     public static void bubbleSort(int[] a) {
         boolean swap = false;
 
+        int firstSortedIndex = a.length;
+        for (int i=0; i<a.length; i++) {
+            for(int j=0; j<firstSortedIndex-1; j++) {   // keeping track of the last value swapped to the sorted
+                if(a[j] > a[j+1]) {                         // logical partition
+                    int temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                    swap = true;
+                }
+            }
+            if(!swap)                   // if no swap occurred while traversing, we know it's sorted and exit
+                break;
+            if(swap) {
+                firstSortedIndex--;
+                swap = false;
+            }
+      }
+    }
+
+    public static void bubbleSort1(int[] a) {
+        boolean swap = false;
+
         for(int i=0; i<a.length; i++) {
             for(int j=0; j<a.length-1; j++) {
                 if(a[j] > a[j+1]) {
