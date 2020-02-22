bc79a23bd4a29fdc2ebdd73d0a2e43a289b04d25
Raul <rcsalaja@us.ibm.com>
2020-02-22 13:26:31 -0600

    WorkLaptop

---
+++ Problems/src/EASY/arithmetic_algorithms/stepsToZero.java
@@ -0,0 +1,19 @@
+package EASY.arithmetic_algorithms;
+
+public class stepsToZero {
+    public static int numberOfSteps (int num) {
+        int steps = 0;
+        while(num!=0) {
+            if(num%2==0)
+                num/=2;
+            else
+                num-=1;
+            steps++;
+        }
+        return steps;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(numberOfSteps(14));
+    }
+}
