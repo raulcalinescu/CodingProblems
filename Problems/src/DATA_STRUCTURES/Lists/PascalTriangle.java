82f8a90b8d1c214883e2b40433f7ca25adfc9736
Raul <rcsalaja@us.ibm.com>
2020-02-25 14:00:40 -0600

    WorkLaptop

---
+++ Problems/src/DATA_STRUCTURES/Lists/PascalTriangle.java
@@ -0,0 +1,29 @@
+package DATA_STRUCTURES.Lists;
+import java.util.ArrayList;
+import java.util.List;
+/*
+Given a non-negative integer numRows, generate the first numRows of Pascal's triangle.
+Example:
+
+Input: 5
+Output:
+[
+     [1],
+    [1,1],
+   [1,2,1],
+  [1,3,3,1],
+ [1,4,6,4,1]
+]
+ */
+public class PascalTriangle {
+    public List<List<Integer>> generate(int numRows) {
+        if (numRows > 2)
+        List<List<Integer>> mainList = new ArrayList<List<Integer>>();
+
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
