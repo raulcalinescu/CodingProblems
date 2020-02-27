2274c006b4f5b2b3eb94f90ecd868f1dc9aa48e6
Raul <rcsalaja@us.ibm.com>
2020-02-27 12:26:23 -0600

    Added PascalTriangle problem to the repository from the work laptop

--- Problems/src/DATA_STRUCTURES/Lists/PascalTriangle.java
+++ Problems/src/DATA_STRUCTURES/Lists/PascalTriangle.java
@@ -17,26 +17,34 @@ Output:
  */
 public class PascalTriangle {
     public static List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> mainList = new ArrayList<List<Integer>>();
-        List<Integer> nums1 = new ArrayList<Integer>();
-        nums1.add(1);
-        mainList.add(nums1);
-        if(numRows == 1)
-            return mainList;
-        List<Integer> nums2 = new ArrayList<Integer>();
-        nums2.add(1);
-        nums2.add(1);
-        mainList.add(nums2);
-        if(numRows == 2)
-            return mainList;
+        List<List<Integer>> triangle = new ArrayList<List<Integer>>();
+        //First base case: if user requests zero rows, they get zero rows. 
+	if(numRows == 0) {
+	    return triangle; 
+    	}
+	//Second base case first row is always [1].
+	triangle.add(new ArrayList<>());
+	triangle.get(0).add(1);
 
-        for(int i=1; i<=numRows; i++) {
-
+	for(int rowNum = 1; rowNum < numRows; rowNum++) {
+	    List<Integer> row = new ArrayList<>();
+	    List<Integer> prevRow = triangle.get(rowNum-1);
+	    //first row element is always 1. 
+	    row.add(1);
+	    //Each triangle element (other than the first and last of each row)
+	    //is equal to the sum of the elements above and to the left, and
+	    //above and to the right. 
+	    for(int j =1; j<rowNum; j++) {
+	        row.add(prevRow.get(j-1)+prevRow.get(j));	
+	    }	
+  	    //the last row element is always 1; 
+	    row.add(1);
+	    triangle.add(row);	
         }
-        return mainList;
+	return triangle;
     }
 
     public static void main(String[] args) {
-        System.out.println(generate(2));
+        System.out.println(generate(3));
     }
 }
