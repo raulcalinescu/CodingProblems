81294ac5df7813a5b2055963b60d0d7fa38e3f61
raulcalinescu <raul.salajan@gmail.com>
2020-03-01 14:05:07 -0600

    Code added from Personal laptop

--- Problems/src/EASY/string_manipulation/longestCommonPrefix.java
+++ Problems/src/EASY/string_manipulation/longestCommonPrefix.java
@@ -19,6 +19,21 @@ Write a function to find the longest common prefix string amongst an array of st
 public class longestCommonPrefix {
 
     public static String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) return "";
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            System.out.println(strs[i].indexOf(prefix));
+            while (strs[i].indexOf(prefix) != 0) {    // returns -1 if can not find index of prefix - means not matching
+                System.out.println(strs[i].indexOf(prefix));
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
+            }
+        }
+        return prefix;
+    }
+
+    //vertical scanning - less space complexity so more optimal than my solution
+    public static String longestCommonPrefixVert(String[] strs) {
         if(strs.length == 0)
             return "";
 
@@ -66,7 +81,7 @@ public class longestCommonPrefix {
     // space complexity is O(1) because we only used constant EXTRA space.
 
     public static void main(String[] args) {
-        String[] strs = {"hello","hell","hellonger"};
+        String[] strs = {"hello","hellob","hellknger"};
     System.out.println(longestCommonPrefix(strs));
     }
 }
