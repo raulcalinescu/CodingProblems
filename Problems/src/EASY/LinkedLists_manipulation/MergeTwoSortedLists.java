c74bacb721f952543aed5b4b400ae9a33252b8fb
raulcalinescu <raul.salajan@gmail.com>
2020-03-09 08:46:32 -0500

    Code added from Personal laptop

--- Problems/src/EASY/LinkedLists_manipulation/MergeTwoSortedLists.java
+++ Problems/src/EASY/LinkedLists_manipulation/MergeTwoSortedLists.java
@@ -18,7 +18,7 @@ public class MergeTwoSortedLists {
         ListNode next;
         ListNode(int x) { val = x; }
     }
-
+    // O(n + m) and space: O(1), only few extra pointers which are constant
     public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if(l1 == null) return l2;
         else if(l2 == null) return l1;
@@ -43,17 +43,29 @@ public class MergeTwoSortedLists {
 
         return head.next;   // return the head of the list from first smallest sorted element
     }
+    // O(n+m) and space O(n+m) - each recursive call takes another stack frame.
+    public static ListNode mergeTwoListsRec(ListNode l1, ListNode l2) {
+        if(l1 == null) return l2;
+        else if (l2 == null) return l1;
+        else if (l1.val < l2.val) {
+            l1.next = mergeTwoListsRec(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoListsRec(l2.next, l1);
+            return l2;
+        }
+    }
 
     public static void main(String[] args) {
         ListNode test1 = new ListNode(1);
-        test1.next = new ListNode(7);
-        test1.next.next = new ListNode(9);
+        test1.next = new ListNode(3);
+        //test1.next.next = new ListNode(9);
 
-        ListNode test2 = new ListNode(1);
-        test2.next = new ListNode(3);
-        test2.next.next = new ListNode(4);
+        ListNode test2 = new ListNode(2);
+        test2.next = new ListNode(4);
+        //test2.next.next = new ListNode(4);
 
-        ListNode result = mergeTwoLists(test1, test2);
+        ListNode result = mergeTwoListsRec(test1, test2);
 
         while(result != null) {
             System.out.print(result.val + " ");
