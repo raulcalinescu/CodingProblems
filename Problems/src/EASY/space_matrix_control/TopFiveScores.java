92d6f6ec13059886316c241575006f839f1f5246
Raul <rcsalaja@us.ibm.com>
2020-05-03 17:59:45 -0500

    WorkLaptop - my solution

--- Problems/src/EASY/space_matrix_control/TopFiveScores.java
+++ Problems/src/EASY/space_matrix_control/TopFiveScores.java
@@ -15,7 +15,7 @@ The average of the student with id = 2 is 88.6. But with integer division their 
 import java.util.*;
 
 public class TopFiveScores {
-    public int[][] highFive(int[][] items) {             //store in a map with ID as the key
+    public static int[][] highFive(int[][] items) {             //store in a map with ID as the key
         Map<Integer, List<Integer>> map = new HashMap<Integer, List<Integer>>();   // and value as ArrayList for scores
         for(int row=0; row<items.length; row++) {
                 if(!map.containsKey(items[row][0]))             // if we haven't seen this ID yet, add an entry for it
@@ -23,16 +23,28 @@ public class TopFiveScores {
                 map.get(items[row][0]).add(items[row][1]);      // else we just add the score to the existing ID
         }
 
-
-
-        return items;
+        int row = 0;
+        int[][] highFive = new int[map.size()][2];   // map.size gives actual size of id's array
+        for(int i : map.keySet()) {
+            int average = 0, divisor = map.get(i).size() < 5? map.get(i).size() : 5;
+            Collections.sort(map.get(i), Collections.reverseOrder()); // use Collections.sort to sort elem in Map
+                                                                        // and reverseOrder to get first elem as Max.
+            for(int j=0; j<divisor; j++) {      // only pick the first 5 or less
+                average += map.get(i).get(j);       // access the value for each key in the map with map.get.get
+            }
+            highFive[row][0] = i;
+            highFive[row][1] = average / divisor;
+            row++;                                      // index to increase rows for the resulting 2D array.
+        }
+        return highFive;
     }
 
     public static void main(String[] args) {
         int[][] a = {{1,91},{1,92},{2,93},{2,97},{1,60},{2,77},{1,65},{1,87},{1,100},{2,100},{2,76}};
+        a = highFive(a);
         for(int row=0; row<a.length; row++) {
             for (int col = 0; col < a[0].length; col++)
-                System.out.println(a[row][col] + " ");
+                System.out.print(a[row][col] + " ");
             System.out.print("\n");
         }
     }
